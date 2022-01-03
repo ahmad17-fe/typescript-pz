bfc316bfaf50df93cf86343861b178676004aa6a
ahmad17-fe <ahmads1704@gmail.com>
2022-01-03 12:35:23 +0700

    belajar array ts

--- tipedata-array.ts
+++ tipedata-array.ts
@@ -9,3 +9,13 @@ console.log("------ARRAY NUMBER------");
 let numbers: Array<number> = [20, 70];
 console.log(numbers);
 console.log("------END ARRAY NUMBER------");
+
+console.log("");
+
+/**
+ * Tupple adalah array yang terbatas dan tipe data nya bisa berbeda-beda
+ */
+console.log("------TUPPLE------");
+let fruit: [string, string, number] = ["Mangga", "Apel", 70];
+console.log(fruit);
+console.log("------END TUPPLE------");
