29403cca6bc5a6444289d9e07cd307bd7f532895
ahmad17-fe <ahmads1704@gmail.com>
2022-01-03 12:26:10 +0700

    belajar tipe data

---
+++ tipedata-array.ts
@@ -0,0 +1,11 @@
+console.log("------ARRAY STRING------");
+let names: string[] = ["Ahmad", "Shobirin"];
+console.log(names);
+console.log("------END ARRAY STRING------");
+
+console.log("");
+
+console.log("------ARRAY NUMBER------");
+let numbers: Array<number> = [20, 70];
+console.log(numbers);
+console.log("------END ARRAY NUMBER------");
