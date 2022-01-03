5b0dcbc7bf4014414c05ba6d6fea791e820f452b
ahmad17-fe <ahmads1704@gmail.com>
2022-01-03 13:18:22 +0700

    belajar tipe data enum

---
+++ tipedata-enum.ts
@@ -0,0 +1,20 @@
+console.log("------ENUM USING NUMBER------");
+enum Gender {
+  Male = 1,
+  Female = 2,
+}
+let jenisKelamin: Gender = Gender.Male;
+console.log(jenisKelamin);
+console.log("------ENUM USING NUMBER------");
+
+console.log(" ");
+
+console.log("------ENUM USING STRING------");
+enum Category {
+  Grocery = "GROCERY",
+  Electronic = "ELECTRONIC",
+  Handphone = "HANDPHONE",
+}
+let kategory: Category = Category.Handphone;
+console.log(kategory);
+console.log("------ENUM USING STRING------");
