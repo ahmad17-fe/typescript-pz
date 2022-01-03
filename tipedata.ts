9135ea8f3462e925f60ea227a0616315991ee301
ahmad17-fe <ahmads1704@gmail.com>
2022-01-03 12:36:06 +0700

    belajar tipe data

---
+++ tipedata.ts
@@ -0,0 +1,22 @@
+console.log("------BOOLEAN------");
+let isStudent: boolean = false;
+isStudent = false;
+console.log(isStudent);
+console.log("------BOOLEAN------");
+
+console.log("");
+
+console.log("------NUMBER------");
+let number: number = 10;
+number += 5;
+console.log(number);
+console.log("------NUMBER------");
+
+console.log("");
+
+console.log("------STRING------");
+let nama: string = "Ahmad";
+nama += " SHOBIRIN";
+let hello = `Hello ${nama}`;
+console.log(hello);
+console.log("------END STRING------");
