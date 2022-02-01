2cd94c6015d1a9206e23b4d1ced6a98953d082f5
ahmad17-fe <ahmads1704@gmail.com>
2022-02-01 10:19:26 +0700

    learn constructor

--- class-ts/class.ts
+++ class-ts/class.ts
@@ -2,8 +2,9 @@ enum Gender {
   Male = "Male",
   Female = "Female",
 }
-class User {
+export class User {
   public id: string;
+
   constructor(public name: string, public age: number, public jk: Gender) {
     const randomNum = (Math.random() * age).toString();
     this.id = randomNum;
@@ -18,15 +19,20 @@ class User {
   }
 }
 
-export default User;
-
-const user = new User("Ahmad", 20, Gender.Male);
-console.log(user);
+// Public = bisa diakses di semua class / dari luar class
+// Protected = hanya bisa diakses dari Class tersebut, dan kelas turunannya
+// Private = hanya bisa diakses dari Class itu sendiri
 
 // Inheritance
 class Admin extends User {
   read: boolean = true;
   write: boolean = true;
+  phone: string;
+
+  constructor(phone:string, name: string, age: number, jk: Gender) {
+    super(name, age, jk);
+    this.phone = phone
+  }
 
   getRole(): { read: boolean; write: boolean } {
     return {
@@ -36,6 +42,6 @@ class Admin extends User {
   }
 }
 
-const admin = new Admin("Ahmad", 20, Gender.Male);
+const admin = new Admin("082187288110","Ahmad", 20, Gender.Male);
 
 console.log(admin);
